405c4ee405df226474009810968f7f1c8316f3b8
sixme <sacondecamacho@gmail.com>
2015-10-10 12:44:59 +0200

    Added selectable next visit date Added delete date button to change the next date
    TO DO: Fix button interaction, they don't work as they should.
                  Correct ticking on timer
                  Update location

--- app/src/main/java/com/santi/loveapp/Map.java
+++ app/src/main/java/com/santi/loveapp/Map.java
@@ -1,98 +1,108 @@
 package com.santi.loveapp;
 
-import android.app.Activity;
-import android.support.v4.app.Fragment;
-
+import android.location.Criteria;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
-import android.view.LayoutInflater;
-import android.view.Menu;
-import android.view.MenuItem;
-import android.view.View;
-import android.view.ViewGroup;
-import android.os.Build;
-import android.widget.Toast;
-import android.util.Log;
+import android.support.v4.app.FragmentActivity;
+import android.widget.TextView;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
-import com.google.android.gms.maps.*;
+import android.util.Log;
 
-public class Map extends Activity {
+import java.text.DecimalFormat;
 
-    /**
-     * Local variables *
-     */
+public class Map extends FragmentActivity implements LocationListener{
+
+    TextView txt;
     GoogleMap googleMap;
+    double fixLat = 42.4338608;
+    double fixLon = -8.6393457;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        System.out.println("VOY00");
-        createMapView();
-        addMarker();
+        setContentView(R.layout.activity_map);
+        SupportMapFragment supportMapFragment =(SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.mapView);
+        googleMap = supportMapFragment.getMap();
+        googleMap.setMyLocationEnabled(true);
+        LocationManager locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
+        Criteria criteria = new Criteria();
+        String bestProvider = locationManager.getBestProvider(criteria, true);
+        Location location = locationManager.getLastKnownLocation(bestProvider);
+        if (location != null) {
+            onLocationChanged(location);
+        }
+        locationManager.requestLocationUpdates(bestProvider, 20000, 0, this);
+
+        addMarker(fixLat,fixLon,"Teddy");
     }
 
-    /**
-     * Initialises the mapview
-     */
-    private void createMapView() {
-        /**
-         * Catch the null pointer exception that
-         * may be thrown when initialising the map
-         */
-        try {
-            if (null == googleMap) {
-                try{
-                    googleMap = ((MapFragment) getFragmentManager().findFragmentById(
-                            R.id.mapView)).getMap();
-                }catch(Exception e){
-                    System.err.println("pete: ");
-                    e.printStackTrace();
-                }
+    private double calculateDistance(double lat1, double lon1, double lat2, double lon2)
+    {
+        double theta = lon1 - lon2;
+        double dist = Math.sin(deg2rad(lat1)) * Math.sin(deg2rad(lat2)) + Math.cos(deg2rad(lat1)) * Math.cos(deg2rad(lat2)) * Math.cos(deg2rad(theta));
+        dist = Math.acos(dist);
+        dist = rad2deg(dist);
+        dist = dist * 60 * 1.1515;
+        dist = dist * 1.609344;
+
+        return (dist);
+    }
 
 
-                /**
-                 * If the map is still null after attempted initialisation,
-                 * show an error to the user
-                 */
-                if (null == googleMap) {
-                    Toast.makeText(getApplicationContext(),
-                            "Error creating map", Toast.LENGTH_SHORT).show();
-                }
-            }
-        } catch (NullPointerException exception) {
-            System.err.println("ERROR");
-            Log.e("mapApp", exception.toString());
-        }
+    private double deg2rad(double deg)
+    {
+        return (deg * Math.PI / 180.0);
+    }
+
+    private double rad2deg(double rad)
+    {
+        return (rad * 180.0 / Math.PI);
     }
 
     /**
      * Adds a marker to the map
      */
-    private void addMarker(){
-
+    private void addMarker(double lat, double lon, String label){
         /** Make sure that the map has been initialised **/
         if(null != googleMap){
-            googleMap.addMarker(new MarkerOptions()
-                            .position(new LatLng(0, 0))
-                            .title("Marker")
-                            .draggable(true)
-            );
+            googleMap.addMarker(new MarkerOptions().position(new LatLng(lat, lon)).title(label).draggable(true));
         }
     }
-/*    *//**
-     * A placeholder fragment containing a simple view.
-     *//*
-    public static class PlaceholderFragment extends Fragment {
 
-        public PlaceholderFragment() {
-        }
+    @Override
+    public void onLocationChanged(Location location) {
+        txt = (TextView) findViewById(R.id.text);
+        double latitude = location.getLatitude();
+        double longitude = location.getLongitude();
+        LatLng latLng = new LatLng(latitude, longitude);
+        //googleMap.addMarker(new MarkerOptions().position(latLng).title("ME").snippet("I am here!"));
+        //googleMap.moveCamera(CameraUpdateFactory.newLatLng(latLng));
+        //googleMap.animateCamera(CameraUpdateFactory.zoomTo(15));
+        DecimalFormat df = new DecimalFormat("#.00");
 
-        @Override
-        public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                                 Bundle savedInstanceState) {
-            View rootView = inflater.inflate(R.layout.fragment_map, container, false);
-            return rootView;
-        }
-    }*/
+        double distance = calculateDistance(fixLat,fixLon,latitude,longitude);
+        txt.setText("We are just: "+ df.format(distance)+" km away!!");
+    }
+
+    @Override
+    public void onProviderDisabled(String provider) {
+        Log.d("Latitude","disable");
+    }
+
+    @Override
+    public void onProviderEnabled(String provider) {
+        Log.d("Latitude","enable");
+    }
+
+    @Override
+    public void onStatusChanged(String provider, int status, Bundle extras) {
+        Log.d("Latitude","status");
+    }
 }
--- app/src/main/java/com/santi/loveapp/NextVisit.java
+++ app/src/main/java/com/santi/loveapp/NextVisit.java
@@ -1,41 +1,114 @@
 package com.santi.loveapp;
 
 import android.app.Activity;
+import android.app.DialogFragment;
 import android.os.CountDownTimer;
 import android.os.Bundle;
 import android.os.Vibrator;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Date;
+import java.util.Locale;
 
 
-public class NextVisit extends Activity {
+public class NextVisit extends Activity implements DatePickerFragment.TheListener{
     protected Vibrator vibrate;
     protected int SECONDS_IN_A_DAY = 24 * 60 * 60;
+    protected String filePath = "";
+    protected SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd");
+    final long[] pattern = {0, 1000, 500, 1000, 500, 1000, 500, 1000 };
+    CountDownTimer timer = null;
+    String dateString = "";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_next_visit);
+        final TextView label = (TextView) findViewById(R.id.label);
         final TextView countdown = (TextView) findViewById(R.id.time_visit);
-        Calendar thatDay = Calendar.getInstance();
-        thatDay.setTime(new Date(0)); /* reset */
-        thatDay.set(Calendar.DAY_OF_MONTH,3);
-        thatDay.set(Calendar.MONTH,6); // 0-11 so 1 less
-        thatDay.set(Calendar.YEAR, 2015);
-        thatDay.set(Calendar.HOUR_OF_DAY, 17);
-        thatDay.set(Calendar.MINUTE,45);
-
+        final Button bDate = (Button) findViewById(R.id.date_button);
+        final Button bDelete = (Button) findViewById(R.id.delete_button);
+        File dir = new File(getFilesDir(), "loveApp");
+        if(!dir.exists())
+            dir.mkdirs();
+        final File f =  new File(getFilesDir()+"/loveApp"+"/love_date.txt");
+        filePath = f.getAbsolutePath();
         vibrate = (Vibrator) getSystemService(VIBRATOR_SERVICE);
-        final long[] pattern = {0, 1000, 500, 1000, 500, 1000, 500, 1000 };
+        bDelete.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(f.exists()) {
+                    f.delete();
+                    System.out.println("File deleted successfully");
+                }
+                    bDate.setVisibility(View.VISIBLE);
+                    label.setText(R.string.next_visit1);
+                    timer.cancel();
+                    countdown.setText("");
+                    System.out.println("I cancelled the timer");
 
-        new CountDownTimer(thatDay.getTimeInMillis(), 1000) {
+                    bDelete.setVisibility(View.INVISIBLE);
+                    bDate.setOnClickListener(new View.OnClickListener() {
+                        public void onClick(View v) {
+                            DialogFragment picker = new DatePickerFragment();
+                            bDelete.setVisibility(View.VISIBLE);
+                            bDate.setVisibility(View.INVISIBLE);
+                            picker.show(getFragmentManager(), "datePicker");
+                        }
+                    });
 
+
+            }
+        });
+
+        if(f.exists()){
+            System.out.println("File exists");
+            bDate.setVisibility(View.INVISIBLE);
+            label.setText(getString(R.string.label));
+            try{
+                BufferedReader br = new BufferedReader(new FileReader(f));
+                dateString = br.readLine();
+                br.close();
+                System.out.println("Date: "+dateString);
+                Date finalDate = formatter.parse(dateString);
+                setCountdown(finalDate);
+            }catch(Exception e){
+                e.printStackTrace();
+                label.setText("ERROR");
+            }
+
+        }else{
+            System.out.println("File DOES NOT EXIST");
+            bDelete.setVisibility(View.INVISIBLE); //Removing the delete date button
+            bDate.setOnClickListener(new View.OnClickListener() {
+                public void onClick(View v) {
+                    DialogFragment picker = new DatePickerFragment();
+                    bDelete.setVisibility(View.VISIBLE);
+                    bDate.setVisibility(View.INVISIBLE);
+                    picker.show(getFragmentManager(), "datePicker");
+                }
+            });
+     }
+    }
+    public void setCountdown(Date finishDate){
+        long end = finishDate.getTime();
+        final TextView countdown = (TextView)  findViewById(R.id.time_visit);
+        timer = new CountDownTimer(end, 1000){
+            @Override
             public void onTick(long millisUntilFinished) {
-                Calendar today = Calendar.getInstance();
-                long diff =  millisUntilFinished - today.getTimeInMillis();
+                long now = Calendar.getInstance(Locale.GERMANY).getTimeInMillis()+1000;
+                long diff =  millisUntilFinished - now;
                 long diffSec = diff / 1000;
 
                 long days = diffSec / SECONDS_IN_A_DAY;
@@ -46,14 +119,34 @@ public class NextVisit extends Activity {
                 countdown.setText(days+ " days, "+ hours + "hours, "+minutes + "m, "+seconds + "s remaining!");
             }
 
+            @Override
             public void onFinish() {
                 countdown.setText("done!");
                 vibrate.vibrate(pattern,-1);
             }
         }.start();
+
+
     }
 
 
+    @Override
+    public void returnDate(String date) {
+        dateString = date;
+        TextView label = (TextView) findViewById(R.id.label);
+        label.setText(R.string.label);
+        BufferedWriter bw;
+        try {
+            bw = new BufferedWriter(new FileWriter(new File(filePath)));
+            bw.write(dateString);
+            bw.close();
+            System.out.println("Saved the file, date is: "+ dateString);
+            Date finishDate = formatter.parse(date);
+            setCountdown(finishDate);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
@@ -76,3 +169,6 @@ public class NextVisit extends Activity {
         return super.onOptionsItemSelected(item);
     }
 }
+
+
+
